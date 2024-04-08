fd92fc283f5d09f87f621ebe05a68a36068879f8
SanderDeKeukelaere <dekeukelaeresander@gmail.com>
2024-04-08 18:01:08 +0200

    Created ImGUI window

---
+++ TaskListManager/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using ClickableTransparentOverlay;
+using ImGuiNET;
+using System.Numerics;
+
+namespace TaskListManager
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var taskListView = new TaskListView();
+            taskListView.Start().Wait();
+        }
+    }
+
+    public class TaskListView : Overlay
+    {
+        protected override void Render()
+        {
+            ImGui.SetNextWindowSize(new Vector2(500, 250));
+            ImGui.Begin("Your task list");
+
+            ImGui.End();
+        }
+    }
+}
