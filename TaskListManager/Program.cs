32af2ed732f01cb92109046cc012845055317277
SanderDeKeukelaere <dekeukelaeresander@gmail.com>
2024-04-08 18:45:31 +0200

    Added tray icon

--- TaskListManager/Program.cs
+++ TaskListManager/Program.cs
@@ -2,6 +2,7 @@
 using ClickableTransparentOverlay;
 using ImGuiNET;
 using System.Numerics;
+using System.Windows.Forms;
 
 namespace TaskListManager
 {
@@ -10,16 +11,58 @@ namespace TaskListManager
         public static void Main()
         {
             var taskListView = new TaskListView();
-            taskListView.Start().Wait();
+            var trayIcon = new TrayIcon
+            {
+                OnClick = taskListView.Show,
+                OnClose = () =>
+                {
+                    Application.Exit();
+                    taskListView.Close();
+                }
+            };
+            taskListView.Start();
+
+            Application.Run();
+        }
+    }
+
+    public class TrayIcon
+    {
+        private NotifyIcon _notifyIcon;
+
+        public Action? OnClick { get; set; }
+        public Action? OnClose { get; set; }
+
+        public TrayIcon()
+        {
+            _notifyIcon = new NotifyIcon();
+            _notifyIcon.Icon = new Icon("Resources/icon.ico");
+            _notifyIcon.Click += (sender, e) => { OnClick?.Invoke(); };
+
+            _notifyIcon.ContextMenuStrip = new();
+            _notifyIcon.ContextMenuStrip.Items.Add("Exit", null, (sender, e) => { OnClose?.Invoke(); });
+
+            _notifyIcon.Visible = true;
         }
     }
 
     public class TaskListView : Overlay
     {
+        private bool _isOpen = true;
+
+        public void Show()
+        {
+            _isOpen = true;
+        }
+
         protected override void Render()
         {
-            ImGui.SetNextWindowSize(new Vector2(500, 250));
-            ImGui.Begin("Your task list");
+            if (!_isOpen) return;
+
+            ImGui.SetNextWindowSize(new Vector2(500,250));
+            ImGui.Begin("Your task list", ref _isOpen);
+
+
 
             ImGui.End();
         }
